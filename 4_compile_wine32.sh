8b2a0691f9740873f218f65067bb4814bde93b98
DanielDevBR <dados.linux@gmail.com>
2020-09-21 19:56:32 -0300

    added libxslt1-dev:i386 just for 32bits

--- 4_compile_wine32.sh
+++ 4_compile_wine32.sh
@@ -16,8 +16,8 @@ echo "* Working inside ${WORKDIR}"
 
 #change packages to 32bits ones, here:
 echo "* Removing some 64bits libs, and add the 32bits ones that have conflits..."
-sudo apt-get -q -y purge libgcrypt20-dev libtiff-dev libcupsimage2-dev libgstreamer-plugins-base1.0-dev libgnutls28-dev libxml2-dev libgtk-3-dev --purge --autoremove >/dev/null || die "* Error apt-get purge to the 32bits change!"
-sudo apt-get -q -y install libgcrypt20-dev:i386 libtiff-dev:i386 libcupsimage2-dev:i386 libgstreamer-plugins-base1.0-dev:i386 libgnutls28-dev:i386 libxml2-dev:i386 libgtk-3-dev:i386 >/dev/null || die "* Error apt-get 32bits!"
+sudo apt-get -q -y purge libgcrypt20-dev libtiff-dev libcupsimage2-dev libgstreamer-plugins-base1.0-dev libgnutls28-dev libxml2-dev libgtk-3-dev libxslt1-dev --purge --autoremove >/dev/null || die "* Error apt-get purge to the 32bits change!"
+sudo apt-get -q -y install libgcrypt20-dev:i386 libtiff-dev:i386 libcupsimage2-dev:i386 libgstreamer-plugins-base1.0-dev:i386 libgnutls28-dev:i386 libxml2-dev:i386 libgtk-3-dev:i386 libxslt1-dev:i386 >/dev/null || die "* Error apt-get 32bits!"
 
 # compile 32bbits
 cd "${WORKDIR}/build32" || die "* Cant enter on the ${WORKDIR}/build32 dir!"
