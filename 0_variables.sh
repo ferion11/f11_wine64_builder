24ca849e2fcf3a4a0d8a810505f583baa1afa1c9
DanielDevBR <dados.linux@gmail.com>
2020-09-25 13:21:16 -0300

    change to tkg 5.17

--- 0_variables.sh
+++ 0_variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export WINE_VERSION="5.17"
 export STAGING_VERSION="${WINE_VERSION}.2"
+export TKG_GIT_COMMIT="4793345595e4bf53abf108f0827ca1ba99de8908"
+export TKG_SRC_FILENAME="wine-tkg-v${WINE_VERSION}-src.tar.gz"
 
 export SDL2_VERSION="2.0.12"
 export FAUDIO_VERSION="20.08"
--- 3_compile_wine64.sh
+++ 3_compile_wine64.sh
@@ -11,16 +11,16 @@ WORKDIR=$(pwd)
 echo "* Working inside ${WORKDIR}"
 
 echo "* Wine part:"
-echo "* Getting wine source and patch..."
-wget -q "https://dl.winehq.org/wine/source/5.x/wine-${WINE_VERSION}.tar.xz"
-tar xf "wine-${WINE_VERSION}.tar.xz" || die "* cant extract wine!"
-mv "wine-${WINE_VERSION}" "wine-src" || die "* cant rename wine-src!"
+echo "* Getting wine source from Tk-Glitch and patch..."
+wget -q "https://github.com/Tk-Glitch/wine-tkg/archive/${TKG_GIT_COMMIT}.tar.gz" -O "${TKG_SRC_FILENAME}"
+tar xf "${TKG_SRC_FILENAME}" || die "* cant extract wine!"
+mv "wine-tkg-${TKG_GIT_COMMIT}" "wine-src" || die "* cant rename wine-src!"
 
 
-wget -q "https://github.com/wine-staging/wine-staging/archive/v${STAGING_VERSION}.tar.gz"
-tar xf "v${STAGING_VERSION}.tar.gz" || die "* cant extract wine-staging patchs!"
+#wget -q "https://github.com/wine-staging/wine-staging/archive/v${STAGING_VERSION}.tar.gz"
+#tar xf "v${STAGING_VERSION}.tar.gz" || die "* cant extract wine-staging patchs!"
 echo "* Applying patchs..."
-"./wine-staging-${STAGING_VERSION}/patches/patchinstall.sh" DESTDIR="${WORKDIR}/wine-src" --all >"${WORKDIR}/staging_patches.txt" || die "* Cant apply the wine-staging patches!"
+#"./wine-staging-${STAGING_VERSION}/patches/patchinstall.sh" DESTDIR="${WORKDIR}/wine-src" --all >"${WORKDIR}/staging_patches.txt" || die "* Cant apply the wine-staging patches!"
 cd "${WORKDIR}/wine-src" || die "Cant enter on ${WORKDIR}/wine-src dir!"
 patch -p1 < "${WORKDIR}/patches/timeout_infinite_fix.patch" || die "Cant apply the timeout_infinite_fix.patch!"
 cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
