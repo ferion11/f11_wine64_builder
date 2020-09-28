a7761aa545a3f96dadf1ecf9aefafcc7f446051f
DanielDevBR <dados.linux@gmail.com>
2020-09-28 10:50:31 -0300

    bugfix hash

--- 0_variables.sh
+++ 0_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export WINE_VERSION="5.18"
 export STAGING_VERSION="${WINE_VERSION}"
-export TKG_GIT_COMMIT="8cb2d6edbf136b8698ad8a8234d6faed825582ae"
+export TKG_GIT_COMMIT="e379f968e8c8e693be4330bfa5ac03595e7ed1ce"
 export TKG_SRC_FILENAME="wine-tkg-v${WINE_VERSION}-src.tar.gz"
 
 export SDL2_VERSION="2.0.12"
