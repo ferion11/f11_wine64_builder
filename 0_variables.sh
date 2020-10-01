ea67011e4c7f7ff9af944333f0452cae495cffd0
DanielDevBR <dados.linux@gmail.com>
2020-10-01 19:33:34 -0300

    testing 3.1 d28c7938c704fe4dcfc8920db494a7175a46fe6b

--- 0_variables.sh
+++ 0_variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #export WINE_VERSION="5.17"
 # using source from: https://github.com/wine-mirror/wine
-# step 3
-export WINE_HASH="a9722145a86ce3392630d469308a9dd731e62e79"
+# step 3.1
+export WINE_HASH="d28c7938c704fe4dcfc8920db494a7175a46fe6b"
 export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
 export STAGING_VERSION="${WINE_VERSION}"
 
