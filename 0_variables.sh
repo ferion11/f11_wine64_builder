162ad686116d0c12383d01fb3aa6bb2977d4a2db
DanielDevBR <dados.linux@gmail.com>
2020-10-20 08:58:21 -0300

    test manual 4 b16f552e195048f434d486a084fad0fa3afcf712

--- 0_variables.sh
+++ 0_variables.sh
@@ -3,8 +3,8 @@
 # using source from: https://github.com/wine-mirror/wine
 # ref with the patch, bad 5.18 d28c7938c704fe4dcfc8920db494a7175a46fe6b
 # ref with the patch, good d28c7938c704fe4dcfc8920db494a7175a46fe6b
-# with patch test Manual-sub 3
-export WINE_HASH="b4c890e37c5d51ee60dd738e5e2ca6dd2e557626"
+# with patch test Manual-sub 4
+export WINE_HASH="b16f552e195048f434d486a084fad0fa3afcf712"
 export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
 export STAGING_VERSION="${WINE_VERSION}"
 
