d44d47e93bb56685926368b92d940186a3668841
DanielDevBR <dados.linux@gmail.com>
2020-10-21 06:10:17 -0300

    test manual i8 6456973f0a64d326bb54da4675310caffc2588f1

--- 0_variables.sh
+++ 0_variables.sh
@@ -3,8 +3,8 @@
 # using source from: https://github.com/wine-mirror/wine
 # ref with the patch, bad 5.18 d28c7938c704fe4dcfc8920db494a7175a46fe6b
 # ref with the patch, good d28c7938c704fe4dcfc8920db494a7175a46fe6b
-# with patch test Manual i7 - side 3b to the first regression log get
-export WINE_HASH="4e11e6e045edd514d553d32ef797cecf98d9a203"
+# with patch test Manual i8
+export WINE_HASH="6456973f0a64d326bb54da4675310caffc2588f1"
 export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
 export STAGING_VERSION="${WINE_VERSION}"
 
