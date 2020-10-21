f8be9f70f38d61a79ede411967550d188ecf2e5f
DanielDevBR <dados.linux@gmail.com>
2020-10-21 16:31:41 -0300

    test manual 5.19

--- 0_variables.sh
+++ 0_variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#export WINE_VERSION="5.17"
+export WINE_VERSION="5.19"
 # using source from: https://github.com/wine-mirror/wine
 # ref with the patch, bad 5.18 d28c7938c704fe4dcfc8920db494a7175a46fe6b
 # ref with the patch, good d28c7938c704fe4dcfc8920db494a7175a46fe6b
-# with patch test Manual i8 - side last git 1
-export WINE_HASH="42bba70a024060fbd698aadec20fbb6f61ad9e16"
-export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
+# with patch test Manual i8 - side 5.19
+#export WINE_HASH="42bba70a024060fbd698aadec20fbb6f61ad9e16"
+#export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
 export STAGING_VERSION="${WINE_VERSION}"
 
 export SDL2_VERSION="2.0.12"
