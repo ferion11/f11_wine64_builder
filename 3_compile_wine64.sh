8d09f8595124afebf8d217e326d086b68c26a39d
DanielDevBR <dados.linux@gmail.com>
2020-10-24 15:47:13 -0300

    test manual i12 5.19 reverting only a404eb7

--- 3_compile_wine64.sh
+++ 3_compile_wine64.sh
@@ -42,7 +42,7 @@ cd "${WORKDIR}/wine-src" || die "Cant enter on ${WORKDIR}/wine-src dir!"
 ##patch -p1 < "${WORKDIR}/patches/tmp_debug_2.patch" || die "Cant apply the tmp_debug_2.patch!"
 #patch -p1 < "${WORKDIR}/patches/tmp_debug_3.patch" || die "Cant apply the tmp_debug_3.patch!"
 #patch -p1 < "${WORKDIR}/patches/tmp_debug_4.patch" || die "Cant apply the tmp_debug_4.patch!"
-patch -p1 < "${WORKDIR}/patches/revert1_b16f552.patch" || die "Cant apply the revert1_b16f552.patch!"
+#patch -p1 < "${WORKDIR}/patches/revert1_b16f552.patch" || die "Cant apply the revert1_b16f552.patch!"
 patch -p1 < "${WORKDIR}/patches/revert2_a404eb7.patch" || die "Cant apply the revert2_a404eb7.patch!"
 cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
 
