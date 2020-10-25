0e0938928b007ca118d8c170a643254e82d6844f
DanielDevBR <dados.linux@gmail.com>
2020-10-25 15:12:38 -0300

    test manual i16 5.19 patch dev1 and 2 and revert 2 commits

--- 0_variables.sh
+++ 0_variables.sh
@@ -3,7 +3,7 @@ export WINE_VERSION="5.19"
 # using source from: https://github.com/wine-mirror/wine
 # ref with the patch, bad 5.18 d28c7938c704fe4dcfc8920db494a7175a46fe6b
 # ref with the patch, good d28c7938c704fe4dcfc8920db494a7175a46fe6b
-# with test Manual i15
+# with test Manual i16
 #export WINE_HASH="148d3aa461126ab6e77e36e33493f87d539cd6ec"
 #export WINE_VERSION="$(echo "${WINE_HASH}" | cut -c1-7)"
 export STAGING_VERSION="${WINE_VERSION}"
