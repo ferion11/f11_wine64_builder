1d60768e1c134cb74dec9eb24aca45500cc6f00a
DanielDevBR <dados.linux@gmail.com>
2021-04-12 07:22:17 -0300

    testing v6.5

--- 3_compile_wine64.sh
+++ 3_compile_wine64.sh
@@ -12,7 +12,7 @@ echo "* Working inside ${WORKDIR}"
 
 echo "* Wine part:"
 echo "* Getting wine source and patch..."
-wget -q "https://dl.winehq.org/wine/source/5.x/wine-${WINE_VERSION}.tar.xz"
+wget -q "https://dl.winehq.org/wine/source/6.x/wine-${WINE_VERSION}.tar.xz"
 tar xf "wine-${WINE_VERSION}.tar.xz" || die "* cant extract wine!"
 mv "wine-${WINE_VERSION}" "wine-src" || die "* cant rename wine-src!"
 
@@ -22,7 +22,7 @@ tar xf "v${STAGING_VERSION}.tar.gz" || die "* cant extract wine-staging patchs!"
 echo "* Applying patchs..."
 "./wine-staging-${STAGING_VERSION}/patches/patchinstall.sh" DESTDIR="${WORKDIR}/wine-src" --all >"${WORKDIR}/staging_patches.txt" || die "* Cant apply the wine-staging patches!"
 cd "${WORKDIR}/wine-src" || die "Cant enter on ${WORKDIR}/wine-src dir!"
-patch -p1 < "${WORKDIR}/patches/timeout_infinite_fix.patch" || die "Cant apply the timeout_infinite_fix.patch!"
+#patch -p1 < "${WORKDIR}/patches/timeout_infinite_fix.patch" || die "Cant apply the timeout_infinite_fix.patch!"
 
 
 cd "${WORKDIR}" || die "Cant enter on ${WORKDIR} dir!"
